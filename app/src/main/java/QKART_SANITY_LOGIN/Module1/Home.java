faeecb00bd5769c17289dd08fa28e0bc71374eac
RADHA BS <bs.radha07@gmail.com>
2024-03-06 17:49:26 +0530

    first commit

---
+++ app/src/main/java/QKART_SANITY_LOGIN/Module1/Home.java
@@ -0,0 +1,38 @@
+package QKART_SANITY_LOGIN.Module1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Home {
+    RemoteWebDriver driver;
+    String url = "https://crio-qkart-frontend-qa.vercel.app";
+
+    public Home(WebDriver driver2) {
+        this.driver = (RemoteWebDriver) driver2;
+    }
+
+    public void navigateToHome() {
+        if (!this.driver.getCurrentUrl().equals(this.url)) {
+            this.driver.get(this.url);
+        }
+    }
+
+    public Boolean PerformLogout() throws InterruptedException {
+        try {
+            // Find and click on the Logout Button
+            WebElement logout_button = driver.findElement(By.className("MuiButton-text"));
+            logout_button.click();
+
+            // Wait for Logout to Complete
+            Thread.sleep(3000);
+
+            return true;
+        } catch (Exception e) {
+            // Error while logout
+            return false;
+        }
+    }
+
+}
