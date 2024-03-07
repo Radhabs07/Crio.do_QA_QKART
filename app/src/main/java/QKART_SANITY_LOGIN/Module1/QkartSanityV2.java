5137ebb41a37d19bb0a8d38148a3658e44e03a8d
RADHA BS <bs.radha07@gmail.com>
2024-03-07 13:22:54 +0530

    Change the directory of Checkout page

--- app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanityV2.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanityV2.java
@@ -22,8 +22,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import QKART_SANITY_LOGIN.Checkout;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class QkartSanityV2 {
